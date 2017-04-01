8cc464bae9b1bed8576576ab58df982d1adb4cbd
Darwin Froese <froese.darwin@gmail.com>
2017-04-01 15:20:10 -0500

    added integration tests and route handling

--- mux/mux.go
+++ mux/mux.go
@@ -45,5 +45,14 @@ func (m *Mux) AddErrorHandler(status int, handler http.HandlerFunc) bool {
 }
 
 func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path
 
+	for _, route := range m.Routes {
+		if route.URL == url {
+			route.Handler(w, r)
+			return
+		}
+	}
+
+	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
---
+++ tests/mux/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/darwinfroese/gowt/mux"
+)
+
+const expectedOutput string = "Hello World"
+
+var srv *httptest.Server
+
+func TestMain(m *testing.M) {
+	setup()
+
+	code := m.Run()
+
+	shutdown()
+
+	os.Exit(code)
+}
+
+func setup() {
+	m := mux.NewMux()
+
+	m.AddRoute("/hello", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, expectedOutput) })
+
+	srv = httptest.NewServer(m)
+}
+
+func shutdown() {
+	srv.Close()
+}
+
+func TestRoute(t *testing.T) {
+	t.Log("Testing Routes")
+
+	res, err := http.Get(srv.URL + "/hello")
+	if err != nil {
+		t.Errorf(" FAILED - %s", err.Error())
+	}
+
+	msg, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Errorf(" FAILED - %s", err.Error())
+	}
+
+	if string(msg) != expectedOutput {
+		t.Errorf(" FAILED - Expected \"%s\" but got \"%s\"", expectedOutput, msg)
+	}
+}
